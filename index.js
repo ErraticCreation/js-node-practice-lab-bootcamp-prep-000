393b7a7dd431ff0b57474e2f65f39fe5c318618d
Tarah Nixon <tarah.sage.nixon@gmail.com>
2018-03-16 18:46:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // write your code below!
 
 let name = "Tarah";
-let height = "70";
+let height = 70;
 
 console.log("Name:");
 console.log(name);
 
 console.log("Height:");
-console.log(parseInt(height) + 1);
+console.log(height + 1);
